f7c2911e9f09a38e9f4c768bdfccacb9e74f2eb1
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2019-10-12 22:07:17 +0200

    Add new patterns (not tested)

--- TrafficLights/TrafficLights.ino
+++ TrafficLights/TrafficLights.ino
@@ -20,9 +20,16 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
 
-// Using an Arduino Uno
+/* Using an Arduino Uno */
+
+// Electronic circuit 
+/////////////////////
 
 #define MODULE_MOSFET // Select MODULE_MOSFET or MODULE_RELAY
+
+
+// Main code
+////////////
 
 #ifdef MODULE_RELAY
 #define INVERT_LIGHT_OUTPUT
@@ -33,13 +40,15 @@ const int GPIO_INTERNAL_LED = 13;
 const int GPIO_LIGHT_GREEN =  10;
 const int GPIO_LIGHT_YELLOW = 11;
 const int GPIO_LIGHT_RED = 12;
+const int GPIO_UNCONNECTED_RANDOM = 0;
 
-const int DURATION_MAIN_LOOP = 100;
-
-const int DURATION_BLINK = 1000;
-const int DURATION_REGULAR_GREEN = 25000;
-const int DURATION_REGULAR_YELLOW = 5000;
-const int DURATION_REGULAR_RED = 25000;
+const unsigned long DURATION_MAIN_LOOP = 100L;         // in ms
+const unsigned long DURATION_REGULAR_BLINK = 1*1000L;  // in ms
+const unsigned long DURATION_REGULAR_GREEN = 60*1000L; // in ms
+const unsigned long DURATION_REGULAR_YELLOW = 5*1000L; // in ms
+const unsigned long DURATION_REGULAR_RED = 60*1000L;   // in ms
+const unsigned long DURATION_CATERPILLAR = 1*1000L;    // in ms
+const unsigned long DURATION_RANDOM = 5*60*1000L;      // in ms
 
 #ifndef INVERT_LIGHT_OUTPUT
 const int LIGHT_ON = HIGH;
@@ -48,20 +57,27 @@ const int LIGHT_OFF = LOW;
 const int LIGHT_ON = LOW;
 const int LIGHT_OFF = HIGH;
 #endif
-
 const int SWITCH_ON = LOW;
 const int SWITCH_OFF = HIGH;
 const int INTERNAL_LED_ON = HIGH;
 const int INTERNAL_LED_OFF = LOW;
 
 void operationAll();
-void operationBlink();
 void operationRegular();
+void operationRegularBlink();
+void operationCaterpillarSimple();
+void operationCaterpillarFilled();
+void operationRandom();
 
 int operationIndex = 0;
 bool operationReset = true;
-const int OPERATION_TABLE_LEN = 3;
-void (*operationTable[])() = {operationAll, operationRegular, operationBlink};
+const int OPERATION_TABLE_LEN = 6;
+void (*operationTable[])() = {
+  operationAll,
+  operationRegular, operationRegularBlink,
+  operationCaterpillarSimple, operationCaterpillarFilled,
+  operationRandom
+};
 
 
 void lightAll() {
@@ -94,6 +110,13 @@ void lightRed() {
   digitalWrite(GPIO_LIGHT_RED, LIGHT_ON);  
 }
 
+int selectRandomGpioLight() {
+  switch(random(0, 3)) {
+    case 0: return GPIO_LIGHT_GREEN;
+    case 1: return GPIO_LIGHT_YELLOW;
+    case 2: return GPIO_LIGHT_RED;
+  }
+}
 
 void operationApply() {
   operationTable[operationIndex]();
@@ -150,7 +173,7 @@ void operationAll() {
 }
 
 
-void operationBlink() {
+void operationRegularBlink() {
   static int state = 0;
   static unsigned long timeRef = millis();
   if (operationReset) {
@@ -164,7 +187,7 @@ void operationBlink() {
       break;
     }
     case 1: {
-      waitAndChangeState(DURATION_BLINK, 2, &timeRef, &state);
+      waitAndChangeState(DURATION_REGULAR_BLINK, 2, &timeRef, &state);
       break;
     }
     case 2: {
@@ -173,7 +196,7 @@ void operationBlink() {
       break;
     }
     case 3: {
-      waitAndChangeState(DURATION_BLINK, 0, &timeRef, &state);
+      waitAndChangeState(DURATION_REGULAR_BLINK, 0, &timeRef, &state);
       break;
     }
   }
@@ -219,6 +242,146 @@ void operationRegular() {
 }
 
 
+void operationCaterpillarSimple() {
+  static int state = 0;
+  static unsigned long timeRef = millis();
+  if (operationReset) {
+    state = 0;
+  }
+  switch(state) {
+    case 0: {
+      lightGreen();
+      timeRef = millis();
+      state = 1;
+      break;
+    }
+    case 1: {
+      waitAndChangeState(DURATION_CATERPILLAR, 2, &timeRef, &state);
+      break;
+    }
+    case 2: {
+      lightYellow();
+      state = 3;
+      break;
+    }
+    case 3: {
+      waitAndChangeState(DURATION_CATERPILLAR, 4, &timeRef, &state);
+      break;
+    }
+    case 4: {
+      lightRed();
+      state = 5;
+      break;
+    }
+    case 5: {
+      waitAndChangeState(DURATION_CATERPILLAR, 0, &timeRef, &state);
+      break;
+    }
+  }
+}
+
+
+void operationCaterpillarFilled() {
+  static int state = 0;
+  static unsigned long timeRef = millis();
+  if (operationReset) {
+    state = 0;
+  }
+  switch(state) {
+    case 0: {
+      digitalWrite(GPIO_LIGHT_GREEN, LIGHT_ON);
+      digitalWrite(GPIO_LIGHT_YELLOW, LIGHT_OFF);
+      digitalWrite(GPIO_LIGHT_RED, LIGHT_OFF);  
+      timeRef = millis();
+      state = 1;
+      break;
+    }
+    case 1: {
+      waitAndChangeState(DURATION_CATERPILLAR, 2, &timeRef, &state);
+      break;
+    }
+    case 2: {
+      digitalWrite(GPIO_LIGHT_GREEN, LIGHT_ON);
+      digitalWrite(GPIO_LIGHT_YELLOW, LIGHT_ON);
+      digitalWrite(GPIO_LIGHT_RED, LIGHT_OFF);  
+      state = 3;
+      break;
+    }
+    case 3: {
+      waitAndChangeState(DURATION_CATERPILLAR, 4, &timeRef, &state);
+      break;
+    }
+    case 4: {
+      digitalWrite(GPIO_LIGHT_GREEN, LIGHT_ON);
+      digitalWrite(GPIO_LIGHT_YELLOW, LIGHT_ON);
+      digitalWrite(GPIO_LIGHT_RED, LIGHT_ON);  
+      state = 5;
+      break;
+    }
+    case 5: {
+      waitAndChangeState(DURATION_CATERPILLAR, 6, &timeRef, &state);
+      break;
+    }
+    case 6: {
+      digitalWrite(GPIO_LIGHT_GREEN, LIGHT_OFF);
+      digitalWrite(GPIO_LIGHT_YELLOW, LIGHT_ON);
+      digitalWrite(GPIO_LIGHT_RED, LIGHT_ON);  
+      state = 7;
+      break;
+    }
+    case 7: {
+      waitAndChangeState(DURATION_CATERPILLAR, 8, &timeRef, &state);
+      break;
+    }
+    case 8: {
+      digitalWrite(GPIO_LIGHT_GREEN, LIGHT_OFF);
+      digitalWrite(GPIO_LIGHT_YELLOW, LIGHT_OFF);
+      digitalWrite(GPIO_LIGHT_RED, LIGHT_ON);  
+      state = 9;
+      break;
+    }
+    case 9: {
+      waitAndChangeState(DURATION_CATERPILLAR, 10, &timeRef, &state);
+      break;
+    }
+    case 10: {
+      digitalWrite(GPIO_LIGHT_GREEN, LIGHT_OFF);
+      digitalWrite(GPIO_LIGHT_YELLOW, LIGHT_OFF);
+      digitalWrite(GPIO_LIGHT_RED, LIGHT_OFF);  
+      state = 11;
+      break;
+    }
+    case 11: {
+      waitAndChangeState(DURATION_CATERPILLAR, 10, &timeRef, &state);
+      break;
+    }
+  }
+}
+
+
+void operationRandom() {
+  static int state = 0;
+  static unsigned long timeRef = millis();
+  if (operationReset) {
+    state = 0;
+  }
+  switch(state) {
+    case 0: {
+      long gpioToChange = selectRandomGpioLight();
+      int currentValue =  digitalRead(gpioToChange);
+      digitalWrite(gpioToChange, !currentValue);
+      timeRef = millis();
+      state = 1;
+      break;
+    }
+    case 1: {
+      waitAndChangeState(DURATION_RANDOM, 0, &timeRef, &state);
+      break;
+    }
+  }
+}
+
+
 void setup() {
   pinMode(GPIO_BUTTON,INPUT_PULLUP);
   pinMode(GPIO_LIGHT_GREEN, OUTPUT);
@@ -228,6 +391,7 @@ void setup() {
   digitalWrite(GPIO_LIGHT_GREEN, LIGHT_OFF);
   digitalWrite(GPIO_LIGHT_YELLOW, LIGHT_OFF);
   digitalWrite(GPIO_LIGHT_RED, LIGHT_OFF);
+  randomSeed(analogRead(GPIO_UNCONNECTED_RANDOM));
 }
 
 
